8cd1b5f88a201a1cd64841b336e3516d4bc30a54
paeksh <sbaek2004@gmail.com>
2026-01-06 23:26:14 +0900

    cnn model 생성

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import tensorflow as tf
-from tensorflow.keras import layers, models
+from tensorflow.keras import layers, models, Sequential
 
-# CNN 모델 생성
-model = models.Sequential([
+##### 1. CNN 모델 생성 #####
+
+CNN_model = models.Sequential([
         layers.Conv2D(32, (3, 3), activation='relu', input_shape=(28, 28, 1)), # convolutional layer
         layers.MaxPooling2D((2, 2)), # pooling layer
         layers.Conv2D(64, (3, 3), activation = 'relu'),
@@ -14,4 +15,13 @@ model = models.Sequential([
 ])
 
 # 모델 요약 출력
-model.summary()
+CNN_model.summary()
+
+##### 2. RNN 모델 생성 #####
+RNN_model = Sequential([
+    layers.SimpleRNN(50, activation = 'tanh', input_shape = (10, 1)), # input sequence = 10, 특성 : 1개
+    layers.Dense(1) # output layer (회귀 예측)
+])
+
+# 모델 요약 출력
+RNN_model.summary()
